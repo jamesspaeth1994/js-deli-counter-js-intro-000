3d631516d437e5d24cbe5007c99e0b30f37382f4
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-08 23:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,3 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
-  
